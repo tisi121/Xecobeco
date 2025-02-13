1e8c1439ce1b3dfc68e6a14d33634e03f58d746d
Jordi Perpiña <thejordiperpina@gmail.com>
2025-02-13 21:04:55 +0100

    He acabat la clase Participant i he fet el 1er cas del Main, el qual es introduir a un nou participant a la llista ja creada

--- Xecobeco/src/xecobeco/Participant.java
+++ Xecobeco/src/xecobeco/Participant.java
@@ -4,12 +4,14 @@
  */
 package xecobeco;
 
+import java.util.Objects;
+
 
 /**
  *
  * @author thejo
  */
-public class Participant {
+public class Participant implements Comparable<Participant>{
     //Constants
     
     //Atributs
@@ -19,11 +21,13 @@ public class Participant {
     private String cognoms;
     private String data_naix;
     private String nom_equip;
+    private char sexe;
     private static int qt;
     
     //Constructor
 
-    public Participant(String dni, String nom, String cognoms, String data_naix, String nom_equip) throws Excepcio1 {
+    public Participant(String dni, String nom, String cognoms, String data_naix, String nom_equip,char sexe) throws Excepcio1 {
+        String[] corredors = {"97", "RUIZ SORIANO, CRISTINA", "43627363F", "5", "43", "56", "14", "4", "1992", "S", "F", "", "89", "FERNÁNDEZ MÁRQUEZ, CARLOS", "45376372J", "6", "45", "46", "17", "7", "1965", "V", "M", "Sherpas de Montserrat", "99", "TORNER ALSINA, MIRIAM", "46384732H", "9", "22", "12", "24", "6", "1961", "V", "F", "", "86", "AGUADO MASCARÓ, ANGELA", "46536384D", "8", "54", "54", "12", "4", "1973", "V", "F", "Relámpagos del suelo", "95", "PUNZANO GOMEZ, MANUEL", "46537628W", "6", "33", "35", "23", "11", "1987", "S", "M", "", "96", "RIERA MARTÍ, JOAN", "46537726S", "5", "55", "0", "5", "2", "1974", "V", "M", "", "99", "TORNER CORRONS, JOAN", "46554444H", "9", "34", "58", "24", "6", "1967", "V", "M", "Sombres de llampecs", "90", "GÓMEZ MARTÍN, ÓSCAR", "46563746T", "8", "55", "49", "19", "12", "1979", "S", "M", "Llebretes i tortugues", "93", "ORTEGA PEDREGOSA, CRISTINA", "46748574D", "7", "33", "1", "2", "6", "1971", "V", "F", "Amb una sabata i una espardenya", "98", "SEGURA ALSINA, POL", "46748993L", "5", "32", "48", "21", "5", "1968", "V", "M", "", "84", "SOLSONA GUISADO, CRISTINA", "46781969S", "5", "20", "48", "30", "8", "1991", "S", "F", "", "87", "BERNAUS ROVIRA, MARC", "46875384T", "6", "33", "46", "6", "3", "1987", "S", "M", "Sherpas de Montserrat", "88", "CAąIZARES GARCÍA, JORDI", "46875384Y", "8", "23", "49", "26", "2", "1981", "S", "M", "Sin prisa pero sin pausa", "26", "CERDÁN DOMINGO, POL", "47079943J", "9", "53", "15", "8", "4", "1983", "S", "M", "La alegria de la huerta", "75", "PARADELL SOLANO, AFRICA", "47110139K", "6", "17", "46", "23", "9", "1990", "S", "F", "Amb una sabata i una espardenya", "81", "SAEZ DELGADO, SANDRA", "47110277K", "6", "14", "35", "14", "2", "1992", "S", "F", "", "78", "PIąOL RODRIGUEZ, ANA", "47110539F", "7", "14", "47", "7", "11", "1964", "V", "F", "", "79", "RIALP GUTIÉRREZ, MARIA LUISA", "47111955C", "7", "9", "1", "23", "6", "1987", "S", "F", "", "72", "NINOT SEGURA, PAULA", "47112432Z", "5", "46", "28", "26", "3", "1981", "S", "F", "Amb una sabata i una espardenya", "76", "PIJOAN PEąA, GEMMA", "47112571S", "8", "43", "49", "6", "4", "1983", "S", "F", "", "74", "PALOMO MOHAMED, SOFIA", "47113927Z", "7", "14", "7", "19", "12", "1979", "S", "F", "Amb una sabata i una espardenya", "69", "HERNÁNDEZ JUSTRIBÓ, SANDRA", "47115806F", "6", "51", "20", "28", "7", "1985", "S", "F", "Pastores de Benasque", "71", "MUąOZ VILA, ESTER", "47117106L", "9", "8", "57", "6", "9", "1987", "S", "F", "Sabatilles BiĽniques", "85", "SURROCA ESTRADA, CAMELIA", "47120856C", "9", "10", "58", "11", "2", "1969", "V", "F", };
         qt++;
         this.dorsal = qt;
         this.setDni(dni);
@@ -31,6 +35,7 @@ public class Participant {
         this.cognoms = cognoms;
         this.data_naix = data_naix;
         this.nom_equip = nom_equip;
+        this.setSexe(sexe);
     }
     
     //Setters
@@ -40,7 +45,7 @@ public class Participant {
             this.dni = dni;
         }
         else{
-            throw new Excepcio1("DNI no valid");
+            throw new Excepcio1(dni+"DNI no valid");
         }
     }
 
@@ -59,11 +64,23 @@ public class Participant {
     public void setNom_equip(String nom_equip) {
         this.nom_equip = nom_equip;
     }
+
+    public void setSexe(char sexe) throws Excepcio1 {
+        if(sexe=='H' || sexe=='D' ||sexe=='h' || sexe=='d'){
+            this.sexe = sexe;
+            
+        }
+        else{
+            throw new Excepcio1("S'ha introduit un sexe incorrecte!!!");
+        }
+        
+    }
+    
     
     //Getters
 
-    public boolean getDni(String dni) {
-        return dni.matches("^\\d{8}[A-Z]$");
+    public String getDni() {
+        return dni;
     }
 
     public String getNom() {
@@ -81,17 +98,29 @@ public class Participant {
     public String getNom_equip() {
         return nom_equip;
     }
+
+    public char getSexe() {
+        return sexe;
+    }
     
     
     //Mètodes
 
     public static boolean validarDni(String dni){
-        //Agafar la part numerica i la lletra
-        String partNumerica=dni.substring(0,8);
-        char lletraDni=dni.charAt(8);
-        
-        char lletraCalculada=calcularLletraDni(partNumerica);
-        return lletraDni==lletraCalculada;
+        if (!dni.matches("^\\d{8}[A-Z]{1}$")) {
+            return false;
+        }
+ 
+        // Extraer la parte numérica y la letra
+        int numero = Integer.parseInt(dni.substring(0, 8));
+        char letra = dni.charAt(8);
+
+        // Calcular la letra correcta
+        String letrasDNI = "TRWAGMYFPDXBNJZSQVHLCKE";
+        char letraCorrecta = letrasDNI.charAt(numero % 23);
+
+        // Comparar la letra proporcionada con la letra correcta
+        return letra == letraCorrecta;
         
     } 
      private static char calcularLletraDni(String partNumerica) {
@@ -107,10 +136,41 @@ public class Participant {
         // Tornar la lletra correspondent
         return letras[resto];
     }
-     
+
     @Override
     public String toString() {
-        return "Participant{" + "dorsal=" + dorsal + ", dni=" + dni + ", nom=" + nom + ", cognoms=" + cognoms + ", data_naix=" + data_naix + ", nom_equip=" + nom_equip + '}';
+        return "Participant{" + "dorsal=" + dorsal + ", dni=" + dni + ", nom=" + nom + ", cognoms=" + cognoms + ", data_naix=" + data_naix + ", nom_equip=" + nom_equip + ", sexe=" + sexe + '}';
     }
+     
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Participant other = (Participant) obj;
+        return Objects.equals(this.dni, other.dni);
+    }
+
+    
+    @Override
+    public int compareTo(Participant o) {
+        return this.dni.compareTo(o.dni);
+    }
+
+    
     
 }
--- Xecobeco/src/xecobeco/Xecobeco.java
+++ Xecobeco/src/xecobeco/Xecobeco.java
@@ -4,6 +4,9 @@
  */
 package xecobeco;
 
+import java.util.Scanner;
+import java.util.TreeSet;
+
 /**
  *
  * @author thejo
@@ -14,10 +17,85 @@ public class Xecobeco {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Excepcio1 {
-        //
-        Participant participant1=new Participant("20565562C","Jordi","Perpiña","21/7/2006","CasiVeo");
-        
-        System.out.println(participant1.toString());
+        //Variables
+        int opcio;
+        Scanner sc = new Scanner(System.in);
+        TreeSet<Participant> participants = new TreeSet<>();
+
+        do {
+            System.out.println("Benvinguts a les inscripcions de la nostra cursa, escolleix una opcio: ");
+            System.out.println("Opcio 1 - Alta Participant");
+            System.out.println("Opcio 2 - Consultar per DNI");
+            System.out.println("Opcio 3 - Eliminar participant per DNI");
+            System.out.println("Opcio 4 - Modificar informacio del participant");
+            System.out.println("Opcio 5- Sortir");
+            opcio = sc.nextInt();
+
+            switch (opcio) {
+                case 1:
+                    //Variables
+                    String dni;
+                    String nom;
+                    String cognoms;
+                    String data_naix;
+                    String nom_equip;
+                    char sexe;
+                    
+                    
+                    System.out.println("Primer de tot voldrem el DNI del participant");
+                    dni = sc.next();
+
+                    boolean dniExisteix = false;
+                    for (Participant p : participants) {
+                        if (p.getDni().equals(dni)) {
+                            dniExisteix = true;
+                            break; // Sortir del bucle si trobem el DNI
+                        }
+                    }
+                    if (dniExisteix) {
+                        System.out.println("Error: El DNI del participant ja està registrat.");
+                    }
+                    System.out.println("Introdueix el nom del participant");
+                    nom = sc.next();
+                    System.out.println("Introdueix els cognoms del participant");
+                    cognoms = sc.nextLine();
+                    System.out.println("Introdueix la data de naixement del participant");
+                    data_naix = sc.nextLine();
+                    System.out.println("Introdueix el nom del equip(si te equip)");
+                    nom_equip = sc.next();
+                    System.out.println("Introdueix el sexe del participant(H/D)");
+                    sexe = sc.next().charAt(0);
+
+                    participants.add(new Participant(dni, nom, cognoms, data_naix, nom_equip, sexe));
+                    System.out.println("Participant introduit correctament");
+                    System.out.println(participants.toString());//Falta ficar els participants de corredors[]
+                    
+                    break;
+                case 2:
+                    //Variables
+                    boolean dniBuscat;
+                    String dniBuscar;
+                    
+                    
+                    System.out.println("Introdueix el DNI del participant");
+                    dniBuscar=sc.next();
+                    for (Participant buscar : participants) {
+                        if (buscar.getDni().equals(dniBuscar)) {
+                            dniBuscat = true;
+                            break; // Sortir del bucle si trobem el DNI
+                        }
+                    }
+                    
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+            }
+
+        } while (opcio != 5);
     }
-    
+
 }
